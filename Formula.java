845c8a93ff572c20556a8d24f41bc5393e44a741
Amarnath-1310 <sivakumaramarnath1310@gmail.com>
2025-05-21 19:25:05 +0530

    deleted

--- Formula.java
+++
@@ -1,5 +0,0 @@
-package functions.JavaPrograms;
-
-public class Formula {
-
-}
--- discount.java
+++
@@ -1,7 +0,0 @@
-package functions.JavaPrograms;
-
-public class discount {
-    public static void main(String[] args) {
-        
-    }
-}
